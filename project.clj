308d892a5df6998faedfdf4733a3437b6d4badd0
Owain Lewis <owain@owainlewis.com>
2016-04-30 17:12:46 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject circle-clj "0.1.0-SNAPSHOT"
   :description "Circle CI"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/owainlewis/circle-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"][clj-http "3.0.1"][cheshire "5.6.1"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "3.0.1"]
+                 [cheshire "5.6.1"]])
